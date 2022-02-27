599599488681c5d7e090e6f4220de18fcef6ec0a
Doug Robison <dfrobison@gmail.com>
2022-02-26 20:11:13 -0700

    Initial Commit

---
+++ dwordle/dwordleApp.swift
@@ -0,0 +1,17 @@
+//
+//  dwordleApp.swift
+//  dwordle
+//
+//  Created by Doug on 2/26/22.
+//
+
+import SwiftUI
+
+@main
+struct dwordleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
